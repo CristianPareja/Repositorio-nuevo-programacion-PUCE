c05d8f637b0b1f0f1020c55a0570fd9eedb52968
watchdog92 <cristhian.pt.1992@gmail.com>
2025-02-11 14:29:23 -0500

    ejercicios Programacion PUCE

--- libreriaKivy.py
+++
@@ -1,8 +0,0 @@
-from libreriaKivy import App
-
-
-class KivySaludo(App):
-    def build(self):
-        return label(text="Hola Mundo")
-    
-KivySaludo().run()
--- repaso examen 3/repaso1.py
+++
@@ -1,9 +0,0 @@
-# presupuestos=["salud","educacion","entretenimiento"]
-# print(presupuestos[0])
-# for presupuesto in presupuestos:
-#     print(presupuesto)
-# for i in range(len(presupuestos)):
-#     print(presupuestos[i])
-# presupuestos=[]
-# presupuestos.append("educacion")
-# print(presupuestos)
